e19d318bbccb121d171fe66544814455de9ea5bc
Serkan-devel <serkan.devel@gmail.com>
2019-01-31 18:47:17 +0000

    begin adding solarized themes

--- css/postprocess.sh
+++ css/postprocess.sh
@@ -5,3 +5,11 @@
 ./postprocess.py m-light.css
 ./postprocess.py m-light.css m-doxygen.css -o m-light+doxygen.compiled.css
 ./postprocess.py m-light.css m-theme-light.css m-doxygen.css --no-import -o m-light.doxygen.compiled.css
+
+./postprocess.py m-solarized.css
+./postprocess.py m-solarized.css m-doxygen.css -o m-light+doxygen.compiled.css
+./postprocess.py m-solarized.css m-theme-solarized.css m-doxygen.css --no-import -o m-solarized.doxygen.compiled.css
+
+./postprocess.py m-solarized_dark.css
+./postprocess.py m-solarized_dark.css m-doxygen.css -o m-light+doxygen.compiled.css
+./postprocess.py m-solarized_dark.css m-theme-solarized_dark.css m-doxygen.css --no-import -o m-solarized_dark.doxygen.compiled.css
